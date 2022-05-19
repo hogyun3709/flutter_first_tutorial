0ec0631ab635fec289da3156f37a889d459512ed
hogyun3709 <hogyun3709@gmail.com>
2022-05-19 12:32:18 +0900

    Custom widget

--- lib/main.dart
+++ lib/main.dart
@@ -10,52 +10,37 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-        home: Scaffold(
-      appBar: AppBar(),
-      body: Container(
-        height: 150,
-        padding: const EdgeInsets.all(10),
+      home: Scaffold(
+        appBar: AppBar(),
+        body: ListView(
+          children: const [
+            Icon(Icons.person_off_rounded)
+          ],
+        ),
+        bottomNavigationBar: const BottomNavigation(),
+      ),
+    );
+  }
+}
+
+class BottomNavigation extends StatelessWidget {
+  const BottomNavigation({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return BottomAppBar(
+      child: SizedBox(
+        height: 70,
         child: Row(
-          children: [
-            ClipRRect(
-              borderRadius: BorderRadius.circular(20.0),
-              child: Container(
-                padding: const EdgeInsets.fromLTRB(10, 0, 10, 0),
-                // If image ratio is diff, define parent w/h and use fit fill(첨부한 이미지의 비율이 다를때)
-                height: 150,
-                width: 150,
-                child: Image.asset(
-                  'emart.jpg',
-                  fit: BoxFit.fill,
-                ),
-              ),
-            ),
-            Expanded(
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  const Text(
-                    '허먼밀러 의자 팝니다',
-                    style: TextStyle(fontSize: 20),
-                  ),
-                  const Text(
-                    '주월 1동',
-                    style: TextStyle(color: Colors.grey, fontSize: 10),
-                  ),
-                  const Text(
-                    '7,000원',
-                    style: TextStyle(fontWeight: FontWeight.bold),
-                  ),
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.end,
-                    children: const [Icon(Icons.favorite), Text('4')],
-                  ), //Row
-                ],
-              ), //Column
-            ),
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          children: const [
+            Icon(Icons.phone),
+            Icon(Icons.message),
+            Icon(Icons.contact_page),
           ],
         ),
       ),
-    ));
+    );
   }
 }
+
